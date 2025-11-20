1e18047a1b6911fd2eae1c0e8cbfb04bb7f60a80
Cunningham Li <2231358@champlaincollege.qc.ca>
2025-11-20 11:47:35 -0500

    Update CourseListViewModel.swift

--- StudyPlan/ViewModels/CourseListViewModel.swift
+++ StudyPlan/ViewModels/CourseListViewModel.swift
@@ -1,1 +1,19 @@
+import SwiftUI
+import SwiftData
+import Combine
 
+@MainActor
+final class CourseListViewModel: ObservableObject {
+    let objectWillChange = ObservableObjectPublisher()
+    
+    func deleteCourses(
+        at offsets: IndexSet,
+        from courses: [Course],
+        in context: ModelContext
+    ) {
+        for index in offsets {
+            context.delete(courses[index])
+        }
+        objectWillChange.send()
+    }
+}
