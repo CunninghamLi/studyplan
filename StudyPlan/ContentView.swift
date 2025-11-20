b37ffb3bcdc63399a05fb2b972092c91f3fa523a
Cunningham Li <2231358@champlaincollege.qc.ca>
2025-11-20 11:49:22 -0500

    Update ContentView.swift

--- StudyPlan/ContentView.swift
+++ StudyPlan/ContentView.swift
@@ -1,1 +1,22 @@
+import SwiftUI
+import SwiftData
 
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            NavigationStack {
+                TodayView()
+            }
+            .tabItem {
+                Label("Today", systemImage: "calendar")
+            }
+            
+            NavigationStack {
+                CourseListView()
+            }
+            .tabItem {
+                Label("Courses", systemImage: "book.closed")
+            }
+        }
+    }
+}
