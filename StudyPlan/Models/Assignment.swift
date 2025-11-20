a7c9dea3dff59ba7ed86142867f81677646e1d2f
Cunningham Li <2231358@champlaincollege.qc.ca>
2025-11-20 11:46:27 -0500

    Update Assignment.swift

--- StudyPlan/Models/Assignment.swift
+++ StudyPlan/Models/Assignment.swift
@@ -1,1 +1,33 @@
+import Foundation
+import SwiftData
+
+@Model
+final class Assignment {
+    var title: String
+    var dueDate: Date
+    var statusRaw: String
+    var notes: String
+
+    @Relationship
+    var course: Course?
+
+    var status: AssignmentStatus {
+        get { AssignmentStatus(rawValue: statusRaw) ?? .notStarted }
+        set { statusRaw = newValue.rawValue }
+    }
+
+    init(
+        title: String,
+        dueDate: Date,
+        status: AssignmentStatus = .notStarted,
+        notes: String = "",
+        course: Course? = nil
+    ) {
+        self.title = title
+        self.dueDate = dueDate
+        self.statusRaw = status.rawValue
+        self.notes = notes
+        self.course = course
+    }
+}
 
