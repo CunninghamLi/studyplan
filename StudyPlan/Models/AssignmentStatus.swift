038de388c0b79274b3c5937472e9a66e75653ce8
Cunningham Li <2231358@champlaincollege.qc.ca>
2025-11-20 11:46:43 -0500

    Update AssignmentStatus.swift

--- StudyPlan/Models/AssignmentStatus.swift
+++ StudyPlan/Models/AssignmentStatus.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+enum AssignmentStatus: String, Codable, CaseIterable, Identifiable {
+    case notStarted = "Not Started"
+    case inProgress = "In Progress"
+    case done = "Done"
+
+    var id: String { rawValue }
+
+    var next: AssignmentStatus {
+        switch self {
+        case .notStarted: return .inProgress
+        case .inProgress: return .done
+        case .done: return .notStarted
+        }
+    }
+}
