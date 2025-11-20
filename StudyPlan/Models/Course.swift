dbede68aa95657c268c8698661b4f21bf9332585
Cunningham Li <2231358@champlaincollege.qc.ca>
2025-11-20 11:47:01 -0500

    Update Course.swift

--- StudyPlan/Models/Course.swift
+++ StudyPlan/Models/Course.swift
@@ -1,1 +1,25 @@
+import Foundation
+import SwiftData
 
+@Model
+final class Course {
+    var name: String
+    var code: String
+    var term: String
+    var colorHex: String
+
+    @Relationship(deleteRule: .cascade, inverse: \Assignment.course)
+    var assignments: [Assignment] = []
+
+    init(
+        name: String,
+        code: String,
+        term: String = "",
+        colorHex: String = "#2E86DE"
+    ) {
+        self.name = name
+        self.code = code
+        self.term = term
+        self.colorHex = colorHex
+    }
+}
